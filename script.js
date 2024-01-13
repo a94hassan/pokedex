8e3bf6d30177dc3b513d529c58d7b6c51eca3ee5
Hassan Ammar <h.ammar@hotmail.de>
2024-01-13 01:49:15 +0100

    Pokemon stats designed and some already linked

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let currentPokemon;
 
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let url = 'https://pokeapi.co/api/v2/pokemon/bulbasaur';
     let response = await fetch(url);
     currentPokemon = await response.json();
     console.log('Loaded pokemon', currentPokemon);
@@ -20,6 +20,7 @@ function renderPokemonInfo() {
     document.getElementById('pokemonId').innerHTML = formattedPokemonId;
     renderPokemonTypes();
     renderPokedexBGColor();
+    renderPokemonStats();
 }
 
 function renderPokemonTypes() {
@@ -41,22 +42,94 @@ function renderPokedexBGColor() {
         'electric': '#eed535',
         'ice': '#51c4e7',
         'fighting': '#d56723',
-
         'ground': '#403233',
-
         'flying': '#005E7C',
-
         'poison': '#b97fc9',
         'psychic': '#f366b9',
         'bug': '#729f3f',
         'rock': '#a38c21',
         'ghost': '#7b62a3',
-
         'dragon': '#0C1618',
-
         'fairy': '#fdb9e9',
         'steel': '#9eb7b8'
     };
     let bgColor = typeColors[currentPokemon['types'][0]['type']['name']];
     document.getElementById('pokedexTop').style = `background-color: ${bgColor}`;
+}
+
+function renderPokemonStats() {
+    let pokemonStats = document.getElementById('pokemonStats');
+    let pokemonHeight = `${calcPokemonHeightInFootAndInch()} (${calcPokemonHeightInMeters()} m)`;
+    let pokemonWeight = `${calcPokemonWeightInPounds()} lbs (${calcPokemonWeightInKilogram()} kg)`;
+    pokemonStats.innerHTML = /*html*/`
+        <table>
+            <tr>
+                <td>Species</td>
+                <td>x</td>
+            </tr>
+            <tr>
+                <td>Height</td>
+                <td>${pokemonHeight}</td>
+            </tr>
+            <tr>
+                <td>Weight</td>
+                <td>${pokemonWeight}</td>
+            </tr>
+            <tr>
+                <td>Abilities</td>
+                <td id="pokemonAbilities"></td>
+            </tr>
+            <th>Breeding</th>
+            <tr>
+                <td>Gender</td>
+                <td>x</td>
+            </tr>
+            <tr>
+                <td>Egg Groups</td>
+                <td>x</td>
+            </tr>
+            <tr>
+                <td>Egg Cycle</td>
+                <td>x</td>
+            </tr>
+        </table>
+    `;
+    renderPokemonAbilities();
+}
+
+function calcPokemonHeightInMeters() {
+    let pokemonHeight = currentPokemon['height'];
+    let pokemonHeightInCentimeters = pokemonHeight * 10;
+    let pokemonHeightInMeters = (pokemonHeightInCentimeters / 100).toFixed(2);
+    return pokemonHeightInMeters;
+}
+
+function calcPokemonHeightInFootAndInch() {
+    let foot = Math.floor(calcPokemonHeightInMeters() * 3.28084);
+    let inch = ((calcPokemonHeightInMeters() * 3.28084 - foot) * 12).toFixed(1);
+    let pokemonHeightInFootAndInch = `${foot}'${inch}"`
+    return pokemonHeightInFootAndInch;
+}
+
+function calcPokemonWeightInKilogram() {
+    let pokemonWeight = currentPokemon['weight'];
+    let pokemonWeightInKilogram = pokemonWeight / 10;
+    return pokemonWeightInKilogram;
+}
+
+function calcPokemonWeightInPounds() {
+    let poundsPerKilogram = 2.20462;
+    let pokemonWeightInPounds = calcPokemonWeightInKilogram() * poundsPerKilogram;
+    return pokemonWeightInPounds.toFixed(1);
+}
+
+function renderPokemonAbilities() {
+    for (let i = 0; i < currentPokemon['abilities'].length; i++) {
+        let pokemonAbility = currentPokemon['abilities'][i]['ability']['name'];
+        let capitalizedPokemonAbility = pokemonAbility.charAt(0).toUpperCase() + pokemonAbility.slice(1);
+        document.getElementById('pokemonAbilities').innerHTML += `${capitalizedPokemonAbility} `;
+        if (i < currentPokemon['abilities'].length - 1) {
+            document.getElementById('pokemonAbilities').innerHTML += ', ';
+        }
+    }
 }
