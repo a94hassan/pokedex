eb8ed611bda29f7ba23276cd07170b80f6c34a90
Hassan Ammar <h.ammar@hotmail.de>
2024-01-14 04:49:40 +0100

    Pokemon overview design almost completed and all infos linked with js

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+let typeColors = {
+    'fire': '#fb6c6c',
+    'water': '#76bdfe',
+    'grass': '#48d0b0',
+    'electric': '#ffd86f',
+    'ground': '#91746B',
+    'poison': '#C288C1',
+    'fairy': '#F3BFD5',
+    'ghost': '#563B63',
+    'dragon': '#44667D',
+    'bug': '#ABE06F',
+
+    'normal': '#C0C2BC',
+    'ice': '#ADCFE2',
+    'fighting': '#E5A66E',
+    'flying': '#005E7C',
+    'psychic': '#D17BE2',
+
+    'rock': '#A9A87D'
+
+  
+
+};
+
 let currentPokemonData;
 
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/9';
+    let url = 'https://pokeapi.co/api/v2/pokemon/6';
     let response = await fetch(url);
     currentPokemonData = await response.json();
     console.log('Loaded pokemon', currentPokemonData);
@@ -33,25 +57,6 @@ function renderPokemonTypes() {
 }
 
 function renderPokedexBGColor() {
-    let typeColors = {
-        'normal': '#a4acaf',
-        'fire': '#fd7d24',
-        'water': '#4592c4',
-        'grass': '#9bcc50',
-        'electric': '#eed535',
-        'ice': '#51c4e7',
-        'fighting': '#d56723',
-        'ground': '#403233',
-        'flying': '#005E7C',
-        'poison': '#b97fc9',
-        'psychic': '#f366b9',
-        'bug': '#729f3f',
-        'rock': '#a38c21',
-        'ghost': '#7b62a3',
-        'dragon': '#0C1618',
-        'fairy': '#fdb9e9',
-        'steel': '#9eb7b8'
-    };
     let bgColor = typeColors[currentPokemonData['types'][0]['type']['name']];
     document.getElementById('pokedexTop').style = `background-color: ${bgColor}`;
 }
@@ -133,40 +138,51 @@ function renderPokemonAbilities() {
     }
 }
 
-
-
-
-
-
-
-let numberOfPokemons = 20;
+let numberOfPokemons = 151;
 let pokemonData;
 
 async function loadPokemons() {
     for (let i = 1; i <= numberOfPokemons; i++) {
       let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
       pokemonData = await response.json();
-      renderOverview();
+      renderOverview(i);
     }
 }
 
-function renderOverview() {
+function renderOverview(i) {
     let pokemonID = pokemonData['id'];
     let formattedPokemonID = "#" + pokemonID.toString().padStart(3, '0');
     let pokemonName = pokemonData['name'];
     let capitalizedPokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+    let pokemonTypes = renderPokemonsTypes(pokemonData['types']);
     let pokemonImage = pokemonData['sprites']['other']['official-artwork']['front_default'];
     let pokemonsOverview = document.getElementById('pokemonsOverview');
-        pokemonsOverview.innerHTML += /*html*/`
-            <div class="pokemonOverview">
-                <h6>${formattedPokemonID}</h6>
-                <h6>${capitalizedPokemonName}</h6>
+    pokemonsOverview.innerHTML += /*html*/`
+        <div class="pokemonOverview" id="pokemonOverview${i}">
+            <h6 class="pokemonIDOverview">${formattedPokemonID}</h6>
+            <div class="pokemonOverviewBottom">
                 <div>
-                    <div>Typen</div>
-                    <img src="${pokemonImage}">
+                    <h6>${capitalizedPokemonName}</h6>
+                    ${pokemonTypes}
                 </div>
+                <img src="${pokemonImage}">
             </div>
-        `;
-
+        </div>
+    `; 
+    renderPokemonOverviewBGColor(i);
 }
 
+function renderPokemonsTypes(types) {
+    let pokemonTypes = '';
+    for (let i = 0; i < types.length; i++) {
+        let type = types[i]['type']['name'];
+        let capitalizedPokemonType = type.charAt(0).toUpperCase() + type.slice(1);
+        pokemonTypes += `<div class="pokemonTypeOverview">${capitalizedPokemonType}</div>`;
+    }
+    return pokemonTypes;
+}
+
+function renderPokemonOverviewBGColor(i) {
+    let bgColor = typeColors[pokemonData['types'][0]['type']['name']];
+    document.getElementById(`pokemonOverview${i}`).style = `background-color: ${bgColor}`;
+}
